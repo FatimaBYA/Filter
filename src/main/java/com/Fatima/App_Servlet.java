8e209b7d006676eafe3402ea1b6eb00f2e297fe1
unknown <fatimaehtp22@gmail.com>
2024-02-19 11:59:00 +0100

    SecondCommit

--- src/main/java/com/Fatima/App_Servlet.java
+++ src/main/java/com/Fatima/App_Servlet.java
@@ -1,5 +1,24 @@
 package com.Fatima;
 
-public class App_Servlet {
+import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class App_Servlet extends HttpServlet{
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.write("<h1>Hellow Servlet with 'web.xml configuration'</h1>");
+		out.write("<h2>Enter yout name</h2>");
+		out.write("<FORM METHOD=GET ACTION='./hello-you'> ");
+		out.println("<input type='text' name='your_name'/>");
+		out.println("<input type='submit' value='ENTER'/>");
+		out.write("</FORM> ");
+		out.close();
+    }
 
 }
